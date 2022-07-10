dba7a8cba626dbc04f72cc3fb9ca934bb6d1969b
OBE (Online Business Engineering) <33532265+oberocks@users.noreply.github.com>
2022-07-10 07:04:35 -0400

    removed safelist from tw config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,9 @@ module.exports = {
         "./index.html",
         "./src/**/*.{vue,js,ts,jsx,tsx}",
         // PROVIDES TAILWIND OBE:WDS DEFAULT DESIGN SYSTEM CLASSES
-        // ---> this NO LONGER will ever work again "./node_modules/@obewds/obewds-tw-config/dist/*.js", <--- !!!
         "./node_modules/@obewds/**/dist/*.js",
         "./obewds.tw.config.json"
     ],
-
-    // safelist: [
-        // Include to ensure TW can parse all app config default classes
-        //JSON.stringify(require('./obewds.tw.config.json'), null, 0),
-    // ],
 
     // theme: {
     //     extend: {},
